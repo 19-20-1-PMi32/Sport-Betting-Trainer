df6e6b6fc111058bcae8aa7ff7d9a5633be0d828
Вова Мільчановський <vovamilchan@gmail.com>
2019-11-03 14:21:45 +0200

    Added interface for repository

---
+++ SBTrainer/SBT.Database/Repository/IGenericRepository.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace SBT.Database.Repository
+{
+    interface IGenericRepository<TEntity> 
+        where TEntity: class
+    {
+        Task<IEnumerable<TEntity>> GetAll();
+
+        Task<TEntity> Get(object id);
+
+        void Insert(TEntity entity);
+
+        void Update(TEntity entity);
+
+        void Delete(object id);
+    }
+}
