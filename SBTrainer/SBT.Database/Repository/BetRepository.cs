78f88c73e0c97c636bd9ccbb7019c841d1fb06ba
Вова Мільчановський <vovamilchan@gmail.com>
2019-11-03 15:24:52 +0200

    Implemented repository interface

---
+++ SBTrainer/SBT.Database/Repository/BetRepository.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+using SBT.Database.Entities;
+
+namespace SBT.Database.Repository
+{
+    public class BetRepository :
+        IGenericRepository<Bet>
+    {
+        private readonly DBContext _dbContext;
+
+        public BetRepository(DBContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<IEnumerable<Bet>> GetAll()
+        {
+            return await _dbContext.Bets.ToListAsync();
+        }
+
+        public async Task<Bet> Get(object id)
+        {
+            return await _dbContext.Bets.FindAsync(id);
+        }
+
+        public void Insert(Bet entity)
+        {
+            _dbContext.Bets.Add(entity);
+        }
+
+        public void Update(Bet entity)
+        {
+            _dbContext.Bets.Update(entity);
+        }
+
+        public async void Delete(object id)
+        {
+            var bet = await _dbContext.Bets.FindAsync(id);
+            if (bet != null)
+            {
+                _dbContext.Bets.Remove(bet);
+            }
+        }
+    }
+}
---
+++ SBTrainer/SBT.Database/Repository/SiteRepository.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+using SBT.Database.Entities;
+
+namespace SBT.Database.Repository
+{
+    public class SiteRepository :
+        IGenericRepository<Site>
+    {
+        private readonly DBContext _dbContext;
+
+        public SiteRepository(DBContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<IEnumerable<Site>> GetAll()
+        {
+            return await _dbContext.Sites.ToListAsync();
+        }
+
+        public async Task<Site> Get(object id)
+        {
+            return await _dbContext.Sites.FindAsync(id);
+        }
+
+        public void Insert(Site entity)
+        {
+            _dbContext.Sites.Add(entity);
+        }
+
+        public void Update(Site entity)
+        {
+            _dbContext.Sites.Update(entity);
+        }
+
+        public async void Delete(object id)
+        {
+            var site = await _dbContext.Sites.FindAsync(id);
+            if (site != null)
+            {
+                _dbContext.Sites.Remove(site);
+            }
+        }
+    }
+}
