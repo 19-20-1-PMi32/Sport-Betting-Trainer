206ceebfe2e6277a1ed5bc3a0313f75c9e711d8b
Calvados0806 <flamaster0807@gmail.com>
2019-11-19 20:35:17 +0200

    Created GemeResult DTO and implemented ParseSports function

---
+++ SBTrainer/SBT.Core/DTO/GameResult.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SBT.Core.DTO
+{
+    class GameResult
+    {
+        public string Team1 { get; set; }
+
+        public string Team2 { get; set; }
+
+        public string Result { get; set; }
+    }
+}
---
+++ SBTrainer/SBT.Core/Parser/Parser.cs
@@ -0,0 +1,38 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+
+namespace SBT.Core.Parser
+{
+    class Parser
+    {
+        public object[] ParseSports(string jsonResponse)
+        {
+            JObject json = JObject.Parse(jsonResponse);
+            if ((bool)json["success"])
+            {
+                List<object> result = new List<object>();
+                foreach (var sportInfo in json["data"])
+                {
+                    result.Add(new {
+                        isActive = (string)sportInfo["active"],
+                        group = (string)sportInfo["group"],
+                        details = (string)sportInfo["details"],
+                        title = (string)sportInfo["title"],
+                        sportId = (string)sportInfo["key"]
+                    });
+                }
+                return result.ToArray();
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public object[] ParseOdds(string jsonResponse)
+        {
+            return new { };
+        }
+    }
+}
